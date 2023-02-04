f41cd0db8602886118b5dc4dd413b64227823e38
PecasDev <juangc1004@hotmail.com>
2023-02-04 14:31:26 -0500

    Ball Behavior Prototype

--- pong.cpp
+++ pong.cpp
@@ -10,6 +10,12 @@ struct LeftPaddle
     int leftPaddle_width{25};
     int leftPaddle_height{125};
     Color leftPaddleColor{WHITE};
+
+    // Left Paddle Edges
+    int LeftPoint_LeftPaddle_X{leftPaddle_x};
+    int RightPoint_LeftPaddle_X{leftPaddle_x + leftPaddle_width};
+    int UpperPoint_LeftPaddle_Y{leftPaddle_y};
+    int BottomPoint_LeftPaddle_Y{leftPaddle_y + leftPaddle_height};
 };
 
 struct RightPaddle
@@ -19,6 +25,12 @@ struct RightPaddle
     int rightPaddle_width{25};
     int rightPaddle_height{125};
     Color rightPaddleColor{WHITE};
+
+    // Right Paddle Edges
+    int LeftPoint_RightPaddle_X{rightPaddle_x};
+    int RightPoint_RightPaddle_X{rightPaddle_x + rightPaddle_width};
+    int UpperPoint_RightPaddle_Y{rightPaddle_y};
+    int BottomPoint_RightPaddle_Y{rightPaddle_y + rightPaddle_height};
 };
 
 struct WindowDimensions
@@ -34,13 +46,29 @@ struct WindowDimensions
     int Y_MAX{665};
 };
 
+struct Ball
+{
+    int ball_x{495};
+    int ball_y{400};
+    int ballRadius{32};
+    Color ballColor{WHITE};
+
+    // Ball Edges
+    int Left_Circle_X{ball_x - ballRadius};
+    int Right_Circle_X{ball_x + ballRadius};
+    int Upper_Circle_Y{ball_y + ballRadius};
+    int Bottom_Circle_Y{ball_y - ballRadius};
+};
+
 int main()
 {
     LeftPaddle leftPaddle;
     RightPaddle rightPadle;
+    Ball ball;
     WindowDimensions windowDimensions;
 
     int paddlesSpeed = 10;
+    int ballSpeed = 5;
 
     int Fps{60};
 
@@ -54,7 +82,8 @@ int main()
         ClearBackground(BLACK);
 
         // Middle line
-        DrawRectangle(490, 0, 10, 850, WHITE);
+        DrawRectangle(494, 0, 6, 850, WHITE);
+        //
 
         // LEFT RECTANGLE
         DrawRectangle(leftPaddle.leftPaddle_x, leftPaddle.leftPaddle_y, leftPaddle.leftPaddle_width, leftPaddle.leftPaddle_height, leftPaddle.leftPaddleColor);
@@ -62,7 +91,7 @@ int main()
         // LEFT PADDLE MOVEMENT
         if (leftPaddle.leftPaddle_y >= windowDimensions.y_min)
         {
-            if (IsKeyDown(KEY_UP))
+            if (IsKeyDown(KEY_W))
             {
                 leftPaddle.leftPaddle_y -= paddlesSpeed;
             }
@@ -70,7 +99,7 @@ int main()
 
         if (leftPaddle.leftPaddle_y <= windowDimensions.Y_MAX)
         {
-            if (IsKeyDown(KEY_DOWN))
+            if (IsKeyDown(KEY_S))
             {
                 leftPaddle.leftPaddle_y += paddlesSpeed;
             }
@@ -82,18 +111,28 @@ int main()
         // RIGHT PADDLE MOVEMENT
         if (rightPadle.rightPaddle_y >= windowDimensions.y_min)
         {
-            if (IsKeyDown(KEY_W))
+            if (IsKeyDown(KEY_UP))
             {
                 rightPadle.rightPaddle_y -= paddlesSpeed;
             }
         }
 
-        if (rightPadle.rightPaddle_y >= windowDimensions.Y_MAX)
+        if (rightPadle.rightPaddle_y <= windowDimensions.Y_MAX)
         {
-            if (IsKeyDown(KEY_S))
+            if (IsKeyDown(KEY_DOWN))
             {
                 rightPadle.rightPaddle_y += paddlesSpeed;
             }
+        }
+
+        // BALL LOGIC
+        DrawCircle(ball.ball_x, ball.ball_y, ball.ballRadius, ball.ballColor);
+
+        ball.ball_y += ballSpeed;
+
+        if (ball.ball_y < windowDimensions.y_min || ball.ball_y > windowDimensions.Y_MAX)
+        {
+            ballSpeed = -ballSpeed;
         }
 
         EndDrawing();
